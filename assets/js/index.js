f7a27d4c714f8fe5f73910e789e98aa3549564dd
CSim <100964977+C-Sim@users.noreply.github.com>
2022-05-02 21:48:34 +0100

    initalise LS and start template strings

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,80 @@
+// global
 const workHours = [
-    {time: 9:00,
-        key: 9,
-    },
-    {time: 10:00,
-        key: 10,
-    },
-    {time: 11:00,
-        key: 11,
-    }, 
-    {time: 12:00,
-        key: 12,
-    }, 
-    {time: 13:00,
-        key: 13,
-    }, 
-    {time: 14:00,
-        key: 14,
-    }, 
-    {time: 15:00,
-        key: 15,
-    },
-    {time: 16:00,
-        key: 16,
-    },
-    {time: 17:00,
-        key: 17,
-    },
-]
+  { time: "9:00", key: 9 },
+  { time: "10:00", key: 10 },
+  { time: "11:00", key: 11 },
+  { time: "12:00", key: 12 },
+  { time: "13:00", key: 13 },
+  { time: "14:00", key: 14 },
+  { time: "15:00", key: 15 },
+  { time: "16:00", key: 16 },
+  { time: "17:00", key: 17 },
+];
+
+const main = $("#main");
+
+// initalise LS
+onReady = () => {
+  // check if tasks exists in LS
+  const tasks = readFromLocalStorage();
+  // if false then set tasks to empty array in LS
+  if (!tasks) {
+    localStorage.setItem("tasks", JSON.stringify([]));
+  }
+};
+
+// get tasks from local storage
+const readFromLocalStorage = () => {
+  // get from LS by key
+  const getTasks = localStorage.getItem("tasks");
+  // parse LS data
+  const parsedData = JSON.parse(getTasks);
+  return parsedData;
+};
+
+renderDate = () => {
+  `<p id="currentDay" class="lead">${moment().format(
+    "MMMM Do YYYY, h:mm:ss a"
+  )}</p>`;
+};
+
+renderTime = () => {};
+
+renderTimeBlocks = (workHours) => {
+  // create block for each time label within object
+  workHours.forEach((hour) => {
+    main.append(`<section
+class="d-inline-flex p-2 bd-highlight justify-content-center align-items-center time-block"
+>
+<span class="hour">${hour.time}</span>
+<div class="p-2 flex-grow-1 bd-highlight textarea">
+// TODO readFromLocalStorage
+</div>
+<div class="buttonContainer">
+  <button type="submit" class="saveBtn">Save</button>
+</div>
+</section>`);
+  });
+};
+
+const writeToLocalStorage = (key, value) => {
+  // stringify object value
+  const stringifiedValue = JSON.stringify(value);
+  // set value for each key within LS
+  localStorage.setItem(key, stringifiedValue);
+};
+
+renderPage = () => {
+  renderDate();
+
+  renderTime();
+
+  renderTimeBlocks();
+};
+
+// event listener for Save button click
+// declare variable for button
+// variable.click(writeToLocalStorage);
+
+// event handler for page load
+$(document).ready(onReady);
